a94d41735fdedeaa9c1ded58789a038e2ada2939
Usuário Convidado <Guest@pa06micro37.local>
2019-02-13 23:05:42 -0200

    aula2

---
+++ MoviesLib/MoviesLib/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 13/02/19.
+//  Copyright © 2019 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
